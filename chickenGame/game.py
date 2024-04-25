c0d18a0e98ce44c80dbcfad4dcad30341979b3b9
dawkasz <daw.dawid.dk@gmail.com>
2024-04-25 16:01:35 -0400

    Changes on april 26th 1 AM
    I added fence placement, chicken movement, chicken's fence detection + behaviour

--- chickenGame/game.py
+++ chickenGame/game.py
@@ -3,7 +3,7 @@ import sys
 import random
 
 from scripts.utils import load_image, load_images, Animation
-from scripts.entities import PhysicsEntity, Chicken
+from scripts.entities import PhysicsEntity, Chicken, Rooster
 from scripts.tilemap import Tilemap
 
 
@@ -16,19 +16,35 @@ class Game:
         self.sidebar = pygame.Surface((560, 720), pygame.SRCALPHA)
         self.clock = pygame.time.Clock()
 
+        self.stages = [60, 80, 100, 140, 180, 240, 300, 400, 500, 600, 720]
+        self.stage = 0
+
+        self.resolution = 60
+
         self.assets = {
             'grass': load_images('tiles/grass'),
+            'fence': load_images('tiles/placeable'),
             'chicken': load_image('entities/chicken.png'),
             'chicken/idle_down': Animation(load_images('entities/chicken/idle_down')),
             'chicken/idle_right': Animation(load_images('entities/chicken/idle_right')),
             'chicken/idle_up': Animation(load_images('entities/chicken/idle_up')),
             'chicken/run_right': Animation(load_images('entities/chicken/run_right')),
             'chicken/run_down': Animation(load_images('entities/chicken/run_down')),
-            'chicken/run_up': Animation(load_images('entities/chicken/run_up'))
+            'chicken/run_up': Animation(load_images('entities/chicken/run_up')),
+            'rooster': load_image('entities/chicken.png'),
+            'rooster/idle_down': Animation(load_images('entities/rooster/idle_down')),
+            'rooster/idle_right': Animation(load_images('entities/rooster/idle_right')),
+            'rooster/idle_up': Animation(load_images('entities/rooster/idle_up')),
+            'rooster/run_right': Animation(load_images('entities/rooster/run_right')),
+            'rooster/run_down': Animation(load_images('entities/rooster/run_down')),
+            'rooster/run_up': Animation(load_images('entities/rooster/run_up'))
         }
 
+        self.selected_fence = 0
+
         self.movement = [False, False, False, False]
-        self.player = Chicken(self, (2, 2), (16, 16))
+
+        self.chickens = []
 
         self.tilemap = Tilemap(self, tile_size=20)
 
@@ -40,41 +56,75 @@ class Game:
 
     def run(self):
         pixels_moved = 0
-        screen_w = 60
-        screen_h = 60
+        size_factor = 720 / self.resolution
+        chicken_movement = []
+
         while True:
             self.screen.fill((0, 0, 0, 0))
             self.game_display.fill((0, 0, 0, 0))
             self.sidebar.fill((90, 0, 0))
 
             self.tilemap.render(self.game_display)
+            self.tilemap.render_fences(self.game_display)
 
-            self.player.update((self.movement[1] - self.movement[0],
-                                self.movement[3] - self.movement[2]))
+            current_tile_img = self.assets['fence'][self.selected_fence].copy()
+            current_tile_img.set_alpha(100)
 
-            if any(self.movement):
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pos = (mouse_pos[0] / size_factor, mouse_pos[1] / size_factor)
+            tile_pos = (int((mouse_pos[0]) // self.tilemap.tile_size),
+                        int((mouse_pos[1]) // self.tilemap.tile_size))
+
+            self.game_display.blit(current_tile_img, (tile_pos[0] * self.tilemap.tile_size,
+                                                      tile_pos[1] * self.tilemap.tile_size))
+
+            if chicken_movement:
                 pixels_moved += 1
+                for i in range(len(self.chickens)):
+                    self.chickens[i].update(chicken_movement[i])
+            else:
+                for chicken in self.chickens:
+                    chicken.update()
             if pixels_moved == 20:
-                self.movement = [False, False, False, False]
+                chicken_movement = []
                 pixels_moved = 0
-            self.player.render(self.game_display)
+            for chicken in self.chickens:
+                chicken.render(self.game_display)
+            self.tilemap.render_front_fences(self.game_display)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    pass
+                if event.type == pygame.MOUSEWHEEL:
+                    if event.y > 0:
+                        self.selected_fence -= 1
+                        if self.selected_fence == -1:
+                            self.selected_fence = 3
+                    elif event.y < 0:
+                        self.selected_fence += 1
+                        if self.selected_fence == 4:
+                            self.selected_fence = 0
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        pos = (int(mouse_pos[0] // self.tilemap.tile_size),
+                               int(mouse_pos[1] // self.tilemap.tile_size))
+                        self.tilemap.place_fence(pos, self.selected_fence)
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_n and pixels_moved == 0:
-                        direction = random.randint(0, 3)
-                        self.movement[direction] = True
+                        for chicken in self.chickens:
+                            movement = self.move_chicken(chicken)
+                            chicken_movement.append(movement)
+                    if event.key == pygame.K_s:
+                        x = int(self.game_display.get_width() / 20)
+                        coordinates = (2 + 20 * random.randint(0, x-1), 2 + 20 * random.randint(0, x-1))
+                        self.chickens.append(Chicken(self, coordinates, (16, 16)))
                     if event.key == pygame.K_r:
-
-                        if screen_w*2 < 960:
-                            screen_w *= 2
-                            screen_h *= 2
-                            self.game_display = pygame.Surface((screen_w, screen_h), pygame.SRCALPHA)
-                            print(screen_w, screen_h)
+                        self.stage += 1
+                        if self.stage > len(self.stages) - 1:
+                            self.stage = 0
+                        self.resolution = self.stages[self.stage]
+                        size_factor = 720 / self.resolution
+                        self.game_display = pygame.Surface((self.resolution, self.resolution))
 
             self.screen.blit(
                 pygame.transform.scale(self.game_display, (720, 720)), (0, 0))
@@ -82,6 +132,27 @@ class Game:
 
             pygame.display.update()
             self.clock.tick(60)
+
+    def move_chicken(self, chicken: Chicken) -> tuple[int, int]:
+        pos = (int(chicken.pos[0] // self.tilemap.tile_size),
+               int(chicken.pos[1] // self.tilemap.tile_size))
+        fences = self.tilemap.get_fences_on_tile(pos)
+        if all([fence == 1 for fence in fences]):
+            return 0, 0
+        direction = random.randint(0, 3)
+        unseen = [0, 1, 2, 3]
+        while fences[direction] != 0:
+            direction = unseen[random.randint(0, len(unseen) - 1)]
+            unseen.remove(direction)
+        if direction == 0:
+            return 0, 1
+        elif direction == 1:
+            return -1, 0
+        elif direction == 2:
+            return 0, -1
+        else:
+            return 1, 0
+
 
 
 Game().run()
--- chickenGame/scripts/tilemap.py
+++ chickenGame/scripts/tilemap.py
@@ -13,37 +13,6 @@ class Tilemap:
         self.tilemap = {}
         self.offgrid_tiles = []
 
-    def extract(self, id_pairs, keep=False):
-        matches = []
-        for tile in self.offgrid_tiles.copy():
-            if (tile['type'], tile['variant']) in id_pairs:
-                matches.append(tile.copy())
-                if not keep:
-                    self.offgrid_tiles.remove(tile)
-
-        for loc in self.tilemap.copy():
-            tile = self.tilemap[loc]
-            if (tile['type'], tile['variant']) in id_pairs:
-                matches.append(tile.copy())
-                matches[-1]['pos'] = matches[-1]['pos'].copy()
-                matches[-1]['pos'][0] *= self.tile_size
-                matches[-1]['pos'][1] *= self.tile_size
-                if not keep:
-                    del self.tilemap[loc]
-
-        return matches
-
-    def tiles_around(self, position):
-        tiles = []
-        tile_loc = (int(position[0] // self.tile_size),
-                    int(position[1] // self.tile_size))
-        for offset in NEIGHBOR_OFFSET:
-            check_loc = (str(tile_loc[0] + offset[0]) + ';'
-                         + str(tile_loc[1] + offset[1]))
-            if check_loc in self.tilemap:
-                tiles.append(self.tilemap[check_loc])
-        return tiles
-
     def save(self, path):
         f = open(path, 'w')
         json.dump({'tilemap': self.tilemap,
@@ -61,21 +30,6 @@ class Tilemap:
         self.tile_size = map_data['tile_size']
         self.offgrid_tiles = map_data['offgrid']
 
-    def solid_check(self, pos):
-        tile_loc = str(int(pos[0] // self.tile_size)) + ';' + str(int(pos[1] // self.tile_size))
-        if tile_loc in self.tilemap:
-            if self.tilemap[tile_loc]['type'] in PHYSICS_TILES:
-                return self.tilemap[tile_loc]
-
-    def physics_rects_around(self, position):
-        rects = []
-        for tile in self.tiles_around(position):
-            if tile['type'] in PHYSICS_TILES:
-                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size,
-                                         tile['pos'][1] * self.tile_size,
-                                         self.tile_size, self.tile_size))
-        return rects
-
     def render(self, surface, offset=(0, 0)):
         for tile in self.offgrid_tiles:
             surface.blit(self.game.assets[tile['type']][tile['variant']],
@@ -90,3 +44,38 @@ class Tilemap:
                     surface.blit(self.game.assets[tile['type']][tile['variant']],
                                  (tile['pos'][0] * self.tile_size - offset[0],
                                   tile['pos'][1] * self.tile_size - offset[1]))
+
+    def render_fences(self, surface, offset=(0, 0)):
+        for x in range(offset[0] // self.tile_size, (offset[0] + surface.get_width()) // self.tile_size + 1):
+            for y in range(offset[1] // self.tile_size, (offset[1] + surface.get_height()) // self.tile_size + 1):
+                loc = str(x) + ';' + str(y)
+                if loc in self.tilemap:
+                    tile = self.tilemap[loc]
+                    for i in range(1, len(tile['fence'])):
+                        if tile['fence'][i] == 1:
+                            surface.blit(self.game.assets['fence'][i],
+                                         (tile['pos'][0] * self.tile_size - offset[0],
+                                          tile['pos'][1] * self.tile_size - offset[1]))
+
+    def render_front_fences(self, surface, offset=(0, 0)):
+        for x in range(offset[0] // self.tile_size, (offset[0] + surface.get_width()) // self.tile_size + 1):
+            for y in range(offset[1] // self.tile_size, (offset[1] + surface.get_height()) // self.tile_size + 1):
+                loc = str(x) + ';' + str(y)
+                if loc in self.tilemap:
+                    tile = self.tilemap[loc]
+                    if tile['fence'][0] == 1:
+                        surface.blit(self.game.assets['fence'][0],
+                                     (tile['pos'][0] * self.tile_size - offset[0],
+                                      tile['pos'][1] * self.tile_size - offset[1]))
+
+    def place_fence(self, pos, f_type):
+        loc = str(pos[0]) + ';' + str(pos[1])
+        if loc in self.tilemap:
+            tile = self.tilemap[loc]
+            tile['fence'][f_type] = 1
+
+    def get_fences_on_tile(self, pos):
+        loc = str(pos[0]) + ';' + str(pos[1])
+        if loc in self.tilemap:
+            tile = self.tilemap[loc]
+            return tile['fence']
